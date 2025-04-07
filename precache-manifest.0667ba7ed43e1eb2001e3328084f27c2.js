6a005a5aefa377faba5a664fe3d0a19f7978b181
Indrasena8 <kindrasena8@gmail.com>
2025-04-06 22:45:28 -0500

    Updates

---
+++ precache-manifest.0667ba7ed43e1eb2001e3328084f27c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b25ebda2dba2e099aa1e3eee9b0fd1c",
+    "url": "/indrasena-portfolio/index.html"
+  },
+  {
+    "revision": "0e91eacf781a837acd11",
+    "url": "/indrasena-portfolio/static/css/main.928072fe.chunk.css"
+  },
+  {
+    "revision": "55fe8d44516cbd7ea581",
+    "url": "/indrasena-portfolio/static/js/2.065b72e2.chunk.js"
+  },
+  {
+    "revision": "1aef9e0d481a6908c30509eb88500877",
+    "url": "/indrasena-portfolio/static/js/2.065b72e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab6136f1a54f4d78a9e9",
+    "url": "/indrasena-portfolio/static/js/3.d20e3203.chunk.js"
+  },
+  {
+    "revision": "0e91eacf781a837acd11",
+    "url": "/indrasena-portfolio/static/js/main.60ed3842.chunk.js"
+  },
+  {
+    "revision": "3b14783e41d89414e6d3",
+    "url": "/indrasena-portfolio/static/js/runtime-main.d97d5d21.js"
+  }
+]);
